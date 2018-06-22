dc87ba601e27e2d43e9b25bcb4e7d59a2c81b075
joshpson <jpearson846@gmail.com>
2018-06-22 13:11:34 -0400

    task refactoring

--- src/task.js
+++ src/task.js
@@ -14,85 +14,90 @@ class Task {
     componentHandler.upgradeElements(element);
   }
 
-  // Make list, add list items, make list items draggable
-
+  // Build List Element
   returnListElement() {
     let li = document.createElement("li");
-    li.id = `list-item-${this.id}`;
     li.setAttribute("data-taskid", this.id);
+    li.id = `list-item-${this.id}`;
     li.className = "mdl-list__item";
     li.draggable = "true";
-    li.addEventListener("dragstart", e => taskDrag(e), false);
-    li.appendChild(this.returnLiSpan());
-    li.appendChild(this.returnCheckBoxSpan(li));
+    this.addListElementListeners(li);
+    li.appendChild(this.returnTitleSpan());
+    li.appendChild(this.returnSwitchButtonSpan());
     return li;
   }
-  // Creating span element, make task editable
-  returnLiSpan() {
+
+  returnTitleSpan() {
     let span = document.createElement("span");
     if (this.status === "Completed") {
       span.style["text-decoration"] = "line-through";
     }
     span.className = "mdl-list__item-primary-content";
+    this.addTaskTitleListeners(span);
     span.innerText = this.description;
-    span.addEventListener("dblclick", () => {
-      span.setAttribute("contenteditable", "true");
-    });
-    span.addEventListener("blur", () => {
-      this.updateDescription(span);
-    });
     return span;
   }
 
-  //Create checkboxes and mark items completed
-  returnCheckBoxSpan(li) {
-    let checkSpan = document.createElement("span");
-    checkSpan.className = "mdl-list__item-secondary-action";
-    let toggleLabel = document.createElement("label");
-    toggleLabel.className = "mdl-switch mdl-js-switch mdl-js-ripple-effect";
-    toggleLabel.setAttribute("for", `switch-1-task-${this.id}`);
-    toggleLabel.appendChild(this.returnCheckBox());
-    checkSpan.appendChild(toggleLabel);
-    return checkSpan;
+  returnSwitchButtonSpan() {
+    let switchButtonSpan = document.createElement("span");
+    switchButtonSpan.className = "mdl-list__item-secondary-action";
+    switchButtonSpan.appendChild(this.returnSwitchButtonLabel());
+    return switchButtonSpan;
   }
 
-  returnCheckBox() {
-    let checkBox = document.createElement("input");
-    checkBox.id = `switch-1-task-${this.id}`;
-    checkBox.className = "mdl-switch__input";
-    checkBox.type = "checkbox";
+  returnSwitchButtonLabel() {
+    let switchButtonLabel = document.createElement("label");
+    switchButtonLabel.className =
+      "mdl-switch mdl-js-switch mdl-js-ripple-effect";
+    switchButtonLabel.setAttribute("for", `switch-1-task-${this.id}`);
+    switchButtonLabel.appendChild(this.returnSwitchButton());
+    return switchButtonLabel;
+  }
+
+  returnSwitchButton() {
+    let switchButton = document.createElement("input");
+    switchButton.id = `switch-1-task-${this.id}`;
+    switchButton.className = "mdl-switch__input";
+    switchButton.type = "checkbox";
     if (this.status === "Completed") {
-      checkBox.checked = true;
+      switchButton.checked = true;
     } else {
-      checkBox.checked = false;
+      switchButton.checked = false;
     }
-    this.addCheckBoxEventListener(checkBox);
-    return checkBox;
+    this.addSwitchButtonListeners(switchButton);
+    return switchButton;
   }
 
-  addCheckBoxEventListener(checkBox) {
-    checkBox.addEventListener("change", e => {
-      if (e.target.checked === true) {
-        this.status = "Completed";
-        setTimeout(() => {
-          this.project.appendActiveTasks();
-        }, 400);
-        patchTask(this);
-      } else {
-        this.status = "Active";
-        setTimeout(() => {
-          this.project.appendCompletedTasks();
-        }, 400);
-        patchTask(this);
-      }
+  //Listeners
+  addListElementListeners(li) {
+    li.addEventListener("dragstart", e => taskDrag(e), false);
+  }
+
+  addTaskTitleListeners(taskTitle) {
+    taskTitle.addEventListener("dblclick", () => {
+      taskTitle.setAttribute("contenteditable", "true");
+    });
+    taskTitle.addEventListener("blur", () => {
+      taskTitle.setAttribute("contenteditable", "false");
+      this.description = taskTitle.innerText;
+      patchTask(this);
     });
   }
 
-  //Update list item descriptions
-  updateDescription(element) {
-    element.setAttribute("contenteditable", "false");
-    this.description = element.innerText;
-    patchTask(this);
+  addSwitchButtonListeners(checkBox) {
+    checkBox.addEventListener("change", e => {
+      setTimeout(() => {
+        if (e.target.checked === true) {
+          this.status = "Completed";
+          this.project.appendActiveTasks();
+          patchTask(this);
+        } else {
+          this.status = "Active";
+          this.project.appendCompletedTasks();
+          patchTask(this);
+        }
+      }, 250);
+    });
   }
 }
 
